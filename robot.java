44de84bfa001bbc7fb163cea0f41f420d8ba01af
Zireha <cheva.wahyU@gmail.com>
2023-05-26 22:35:57 +0700

    update on class robot

--- robot.java
+++ robot.java
@@ -8,6 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class robot extends Actor
 {
+    public robot()
+    {
+        GreenfootImage myImage = getImage();
+        myImage.scale(72, 72);
+    }
+    
     /**
      * Act - do whatever the robot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +21,14 @@ public class robot extends Actor
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            setLocation(getX()-5, getY());
+        }else
+        if(Greenfoot.isKeyDown("right"))
+        {
+            setLocation(getX()+5, getY());
+        }
     }
+
 }
